f1c7564c3c80b4d029344b3a832a851b477de3b8
Damien Brun <makowildcat@gmail.com>
2019-05-24 00:11:43 -0400

    Create ReadSerial script

---
+++ Assets/Scripts/ReadSerial.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+public class ReadSerial : MonoBehaviour 
+{
+	public enum Baudrate 
+	{
+  		b300 = 300, b600 = 600, b1200 = 1200, b2400 = 2400, b4800 = 4800, b9600 = 9600, b14400 = 14400, b19200 = 19200, b28800 = 28800, b38400 = 38400, b57600 = 57600, b115200 = 115200
+	}
+
+	public event Action<string> OnRead = delegate { };
+
+	private SerialPort _serialPort;
+	public string port;
+	public Baudrate baudrate = Baudrate.b9600;
+
+	[Tooltip("Value in milliseconds.")]
+	public int readTimeout = 500;
+	
+	[Tooltip("Value in milliseconds.")]
+	public int writeTimeout = 500;
+	private bool _continue = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Thread readThread = new Thread(Read);
+
+		_serialPort = new SerialPort(port, Convert.ToInt32(baudrate));
+        _serialPort.ReadTimeout = readTimeout;
+        _serialPort.WriteTimeout = writeTimeout;
+        _serialPort.Open();
+
+		_continue = true;
+
+		readThread.Start();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Read()
+    {
+        while (_continue)
+        {
+            try
+            {
+                string message = _serialPort.ReadTo(".");
+                UnityMainThreadDispatcher.Instance().Enqueue(ThisWillBeExecutedOnTheMainThread(message));
+            }
+            catch (TimeoutException)
+            {
+
+            }
+        }
+    }
+
+	public IEnumerator ThisWillBeExecutedOnTheMainThread(string message)
+    {
+		Debug.Log(message);
+
+		OnRead(message);
+		
+        yield return null;
+    }
+}
