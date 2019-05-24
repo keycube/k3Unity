28afa5d9171b2643fc6ac66e1e4dc1d0b43e8803
Damien Brun <makowildcat@gmail.com>
2019-05-24 19:02:41 -0400

    Create Keycube script

---
+++ Assets/Scripts/Keycube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keycube : MonoBehaviour {
+
+	public ReadSerial readSerial;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (readSerial != null) 
+		{
+			readSerial.OnRead += ReadSerial_OnRead;
+		}
+	}
+	
+	
+	private void ReadSerial_OnRead(string message)
+	{
+		Debug.Log("Keycube: " + message);
+	}
+}
